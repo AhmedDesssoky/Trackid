c2a9c9ae379bd67ad188c0e36723cf235266d822
Ahmeddessoky <ahmeddessoky552001@gmail.com>
2023-07-10 18:25:17 +0300

    fina2

--- src/components/SearchForMissing/SearchForMissing.vue
+++ src/components/SearchForMissing/SearchForMissing.vue
@@ -137,7 +137,7 @@ export default {
 
         axios
           .post(
-            "https://75dc-156-196-224-32.eu.ngrok.io/recognize_faces",
+            "https://6935-156-196-224-32.eu.ngrok.io/recognize_faces",
             formData
           )
           .then((response) => {
